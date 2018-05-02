05694ed9841f652aa9ad126c2617b92853bc0035
Rylan Branch <rylan.branch@getg5.com>
2018-05-02 18:54:44 +0000

    update devise recovery emailer

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'simple_form'
 gem 'bootstrap-sass', '~> 3.3.7'
 gem 'sass-rails', '>= 3.2'
+gem 'devise'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,44 +1,55 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   
   def index
     @post = Post.all
   end
   
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
   
   def create
-    @post = Post.create(post_params)
-    
-    redirect_to posts_path
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to root_path
+      flash[:success] = "Post created!"
+    else
+      flash.now[:alert] = "Post was not created, please check the form."
+      render :new
+    end
   end
   
   def show
-    @post = Post.find(params[:id])
   end
   
   def edit
-    @post = Post.find(params[:id])
   end
   
   def update
-      @post = Post.find(params[:id])
-      
-      @post.update(post_params)
-      
+    if @post.update(post_params)
+      flash.now[:success] = "Post updated."
       redirect_to post_path(@post)
+    else
+      flash.now[:alert] = "Update failed, please check the form."
+      render :edit
+    end
   end
   
   def destroy
-    @post = Post.find(params[:id])
-    @post.destroy!
-    redirect_to posts_path
+    @post.destroy
+    redirect_to root_path
+    flash[:success] = "Post deleted."
   end
   
   private
   
+    def set_post
+      @post = Post.find(params[:id])
+    end
+  
     def post_params
-      params.require(:post).permit(:image, :caption)
+      params.require(:post).permit(:image, :caption, :user_id)
     end
 end
